49ef55c9ed44f462f9696ad2dbdbaf6586a1d7c9
PebbleDevelopment <mnashesduna@gmail.com>
2025-03-12 20:57:04 +0200

    initialise app

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen w-full">
+      yeah
+    </div>
+  );
+}
