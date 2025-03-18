22fdce8280183ce2adf64bfbb8e5bd335bc05c94
Denis Siduna <125746473+munaashe@users.noreply.github.com>
2025-03-18 14:57:39 +0200

    update bg and text dark mode colors

--- components/banner/plain-bg.tsx
+++ components/banner/plain-bg.tsx
@@ -6,7 +6,7 @@ interface BannerProps {
 
 const PlainBgBanner: React.FC<BannerProps> = ({ title, subtitle }) => {
     return (
-        <div className="bg-gray-100 p-8 text-center h-[540px] flex items-center justify-center flex-col dark:text-black">
+        <div className="bg-gray-100 dark:bg-gray-700 p-8 text-center h-[540px] flex items-center justify-center flex-col text-black dark:text-white">
             <h1 className="text-4xl font-bold mb-4">{title}</h1>
             <p className="text-lg">{subtitle}</p>
         </div>
